93f02fae68634e3fbe5f7e5a52b701b5fe62054c
Célio Santiago <celio.santiago.700@ufrn.edu.br>
2025-01-13 16:53:14 -0300

    oi
    oi

---
+++ src/main/java/com/exemplo/biscoitosorte/BiscoitoSorteApplication.java
@@ -0,0 +1,13 @@
+package com.exemplo.biscoitosorte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiscoitoSorteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiscoitoSorteApplication.class, args);
+	}
+
+}
